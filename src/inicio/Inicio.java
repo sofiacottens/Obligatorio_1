bc8aa2bdf61adfdb8f2acd8593fa6390db11c3f8
sofiacottens <82243983+sofiacottens@users.noreply.github.com>
2022-11-13 16:10:17 -0300

    Merge origin/sofi into HEAD
    Conflicts:
    	src/dominio/Sistema.java
    	src/dominio/sistema/PuestoDeTrabajo.java
    	src/dominio/sistema/Sector.java
    	src/dominio/trabajador/SistemaTrabajador.java
    	src/uiswing/LoginAdministrador.java
    	src/uiswing/LoginTrabajador.java
    	test/unit_test/UserUnitTest.java

--- src/inicio/Inicio.java
+++ src/inicio/Inicio.java
@@ -3,14 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package inicio;
+import dominio.Sector.SectorException;
 import uiswing.Principal;
 
 public class Inicio {
 
     /**
      * @param args the command line arguments
+     * @throws dominio.Sector.SectorException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SectorException {
         DatosPrueba.cargar();
         Principal principal = new Principal();
         principal.setVisible(true);
